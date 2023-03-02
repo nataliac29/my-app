a862fa3b734f1f04cfb881b67bb0159f718b5384
Natalia Calvo <calvonatalia1@gmail.com>
2023-03-02 18:09:32 -0500

    works but opens new page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,18 @@
 <!-- CODE ADAPTED FROM: https://svelte.dev/repl/801ec208ac3a44a5880ea4c31703b39f?version=3.45.0 -->
 <script>
+	import Modal from "./Modal.svelte";
+
+	let showModal = true;
+
 	let columns = new Array(7);
 	let rows = new Array(30);
 	let state = new Array(rows.length * columns.length).fill("");
 	let isDrag = false;
 	let isYard = true;
 	let isSeas = false;
+	let endTime = null;
+	let startTime = null;
+	let timer = 0;
 
 	const beginDrag = () => {
 		isDrag = true;
@@ -35,6 +42,20 @@
 			toggle(r, c);
 		}
 	};
+
+	const clickHandler = () => {
+		showModal = true;
+		endTime = Date.now();
+		// console.log("HERE, DONE CLICK HANDLER");
+		// console.log("START TIME");
+		// console.log(startTime);
+		// console.log("END TIME");
+		// console.log(endTime);
+		let difference = Math.abs(endTime - startTime);
+		timer = difference / 1000;
+		// console.log("DIFFERENCE IN SECONDS");
+		// console.log(timer);
+	};
 </script>
 
 <svelte:head>
@@ -47,6 +68,31 @@
 	on:mouseup={endDrag}
 	on:keypress={(e) => e.key == "q" && changeLocation()}
 />
+<form
+	class="gform"
+	method="POST"
+	data-email="ncalvo@college.harvard.edu"
+	action="https://script.google.com/macros/s/AKfycbxmP2lKcfwf10uL-Ghknowoe2S-E9IHtFY5ZhQrG1QCNQdK-ucPqE4QUud_oHDFrE6pdA/exec"
+>
+	<fieldset class="pure-group">
+		<textarea id="message" name="message" rows="0" value={timer} />
+		>
+	</fieldset>
+	<button on:click={clickHandler} type="submit"> DONE </button>
+	<script
+		data-cfasync="false"
+		type="text/javascript"
+		src="form-submission-handler.js"
+	></script>
+</form>
+
+<Modal bind:showModal bind:startTime>
+	<p>
+		Click the "Start Button" to start the time, and when you're done, click the
+		"done" button to stop the time
+	</p>
+</Modal>
+
 <div class:container={true}>
 	<button on:click={changeLocation} class:button-selected={isYard}>
 		YARD
