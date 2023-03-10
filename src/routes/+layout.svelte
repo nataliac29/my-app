c964b28b1939aee1833153ccee52022085beb2a6
patshiab <55057047+patshiab@users.noreply.github.com>
2023-03-10 15:20:28 -0500

    finished!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,11 @@
 </script>
 
 <div class="app">
-	<!-- <Header /> -->
 
 	<main>
 		<slot />
 	</main>
 
-	<!-- <footer>
-		<p>
-			visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-		</p>
-	</footer> -->
 </div>
 
 <style>
@@ -36,21 +30,7 @@
 		box-sizing: border-box;
 	}
 
-	/* footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	} */
-
-	footer a {
-		font-weight: bold;
-	}
 
 	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
